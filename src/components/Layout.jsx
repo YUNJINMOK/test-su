5391c6e4fae2bb95e7927c9ebe8b3aaed52d81f2
zxwgm <aprnaprn114@naver.com>
2024-05-07 13:56:54 +0900

    re

---
+++ src/components/Layout.jsx
@@ -0,0 +1,13 @@
+import React, { Children } from "react";
+import Header from "./Header";
+import MenuBar from "./MenuBar";
+
+export default function Layout(children) {
+  return (
+    <div>
+      <Header />
+      <div>{children}</div>
+      <MenuBar />
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import MainPage from './router/MainPage';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import MainPage from "./router/MainPage";
+import Home from "./router/Home";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainPage />,
+    errorElement: "",
+    children: [
+      {
+        path: "/home",
+        element: <Home />,
+      },
+    ],
+  },
+]);
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <MainPage />
