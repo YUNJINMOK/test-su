facac6084f0dc1cc17c8715e77e1ea7f3c2c830c
schentea <schentea@gmail.com>
2024-05-07 13:31:24 +0900

    1

---
+++ src/components/MenuBar.jsx
@@ -0,0 +1,7 @@
+import "../style/menubar";
+
+export default function MenuBar() {
+  return <div>
+    
+  </div>;
+}
--- src/router/MainPage.jsx
+++ src/router/MainPage.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "../style/main.css";
-import MainLogo from "../assets/sumokwonLogo.png";
+import MainLogo from "../assets/gpsLogoWhite.svg";
 
 export default function MainPage() {
   return (
     <>
-      <div className="main-bg"></div>
       <div className="main-con">
+        \
         <img className="main-logo" src={MainLogo} alt="메인 로고" />
         <div className="main-title">대구 수목원</div>
       </div>
