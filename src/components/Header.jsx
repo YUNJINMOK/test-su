0a5c3d2a3a9c8fc4e60877e0e8c5634c96c28993
ldh <ai04120@naver.com>
2024-05-08 11:35:04 +0900

    1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { IoIosArrowBack } from "react-icons/io";
 import headerLogo from "../assets/gpsLogo.svg";
+import { useNavigate } from "react-router-dom";
 
 export default function Header() {
+  const navi = useNavigate();
   return (
-    <div className="w-full h-[60px] flex  justify-center">
+    <div className="w-full h-[60px] flex justify-center">
       <div className="w-full max-w-[400px]  h-full flex items-center">
-        <div className="w-[20%] ml-4 flex justify-start items-center">
+        <div className="w-[20%] ml-4 flex justify-start items-center" onClick={() => navi(-1)}>
           <IoIosArrowBack />
         </div>
         <img className="w-[15%] h-[50%]" src={headerLogo} alt="logo" />
--- src/router/MyPage.jsx
+++ src/router/MyPage.jsx
@@ -1,10 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../components/Layout";
+import "../style/mypage.css";
 
 export default function MyPage() {
+  const [btn1, setBtn1] = useState(false);
+  const [btn2, setBtn2] = useState(false);
+
   return (
     <Layout>
-      <div>MyPage</div>
+      <section>
+        <h2>ooo123 님의 마이페이지</h2>
+        <div id="accountInfo">
+          <div>
+            <p>아이디</p>
+            <p>ooo123</p>
+          </div>
+          <div>
+            <div>
+              <p>비밀번호</p>
+              <p>******</p>
+            </div>
+            <button>수정</button>
+          </div>
+        </div>
+      </section>
+      <div className="settingBtn" onClick={() => setBtn1(!btn1)}>
+        <div className="btnTxt">
+          <p>다크 모드</p>
+          <p>앱 화면을 어둡게 변경합니다.</p>
+        </div>
+        <div className="togBtn">
+          <div className="togCircle" style={{ left: btn1 ? "22px" : "0" }}></div>
+        </div>
+      </div>
+      <div className="settingBtn" onClick={() => setBtn2(!btn2)}>
+        <div className="btnTxt">
+          <p>버튼 이름</p>
+          <p>버튼 설명이 들어오는 곳</p>
+        </div>
+        <div className="togBtn">
+          <div className="togCircle" style={{ left: btn2 ? "22px" : "0" }}></div>
+        </div>
+      </div>
     </Layout>
   );
 }
