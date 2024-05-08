f4e0d54057930a262157ef810c0e395851a4ba10
ldh <ai04120@naver.com>
2024-05-08 10:00:08 +0900

    1

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -1,13 +1,22 @@
+import { Link } from "react-router-dom";
 import "../style/menubar.css";
 import Icon from "./Icon";
 
 export default function MenuBar() {
   return (
-    <div className=" fixed bottom-0 w-full h-[70px] bg-[#ECECEC] flex items-center justify-around">
-      <Icon text="홈" />
-      <Icon text="지도" />
-      <Icon text="스템프" />
-      <Icon text="마이페이지" />
-    </div>
+    <nav className=" fixed bottom-0 w-full h-[70px] bg-[#ECECEC] flex items-center justify-around">
+      <Link to="/home">
+        <Icon text="홈" />
+      </Link>
+      <Link>
+        <Icon text="지도" />
+      </Link>
+      <Link to="/stamp">
+        <Icon text="스템프" />
+      </Link>
+      <Link>
+        <Icon text="마이페이지" />
+      </Link>
+    </nav>
   );
 }
