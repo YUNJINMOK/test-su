64c8e556e70cc2425ceaf2a480807cf106dfba04
Kimeunryeong <ker26772@gmail.com>
2024-05-09 09:52:50 +0900

    ㄴㅅ므ㅔ

--- src/router/StampPage.jsx
+++ src/router/StampPage.jsx
@@ -9,7 +9,7 @@ export default function StampPage() {
         {/* 드롭다운 */}
         <div className="hs-dropdown relative inline-flex">
           <button id="hs-dropdown-default" type="button" className="hs-dropdown-toggle py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none">
-            Actions
+            전체보기
             <svg className="hs-dropdown-open:rotate-180 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round">
               <path d="m6 9 6 6 6-6" />
             </svg>
@@ -17,17 +17,15 @@ export default function StampPage() {
 
           <div className="hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-60 bg-white shadow-md rounded-lg p-2 mt-2 after:h-4 after:absolute after:-bottom-4 after:start-0 after:w-full before:h-4 before:absolute before:-top-4 before:start-0 before:w-full" aria-labelledby="hs-dropdown-default">
             <a className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100" href="#">
-              Newsletter
+              전체보기
             </a>
             <a className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100" href="#">
-              Purchases
+              완료
             </a>
             <a className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100" href="#">
-              Downloads
+              미완료
             </a>
-            <a className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100" href="#">
-              Team Account
-            </a>
+            
           </div>
         </div>
         {/* 스탬프 */}
