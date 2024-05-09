ce8564e342749b17f666a662b4fd4c145b206146
Kimeunryeong <ker26772@gmail.com>
2024-05-09 13:15:23 +0900

    asdf

--- src/router/QrPage.jsx
+++ src/router/QrPage.jsx
@@ -6,12 +6,10 @@ import { Link } from "react-router-dom";
 export default function QrPage() {
   return (
     <div className="qrSection">
-      <Link to="/home" className="qrArrow" >
+      <Link to="/home" className="qrArrow">
         <IoIosArrowBack color="white" />
       </Link>
-
       <p className="qrText">QR 코드를 촬영해주세요</p>
-
       <div className="qrZone">
         <p>QR 카메라보이게해주셍</p>
       </div>
