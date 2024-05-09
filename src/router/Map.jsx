7f94aad37ba052319eb5fe42a58176107a95d9ae
Kimeunryeong <ker26772@gmail.com>
2024-05-09 09:50:23 +0900

    stamp

--- src/router/Map.jsx
+++ src/router/Map.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from "react";
+import Layout from "../components/Layout";
 
 export default function Map() {
   return (
-    <div>지도</div>
-  )
+    <Layout>
+      <div>지도</div>
+    </Layout>
+  );
 }
