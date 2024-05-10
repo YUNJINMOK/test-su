218ac720248531c617881049ffd14e8bc43d72af
schentea <schentea@gmail.com>
2024-05-10 15:15:21 +0900

    Register

---
+++ src/api.js
@@ -0,0 +1,29 @@
+const BASE_URL = process.env.REACT_APP_BASE_URL
+//스탬프 test
+export async function setStamp(stamp_id) {
+  try {
+    return await fetch(`http://localhost:8000/test`, {
+      method :"POST",
+      headers: {
+        'Content-Type' :'application/json'
+      },
+      body : JSON.stringify({ stamp_id: stamp_id, user_id : 1 })
+    }).then((res) => res.json())
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+//회원가입 요청
+export async function apiPostUserRegiser(data) {
+  console.log(data)
+  try {
+    return await fetch(`${BASE_URL}/users/register`, {
+      method : "POST",
+      headers : {"Content-Type": "application/json"},
+      body : JSON.stringify(data)
+    }).then((res) => res.json())
+  } catch (error) {
+    console.log(error)
+  }
+}
--- src/router/SignUp.jsx
+++ src/router/SignUp.jsx
@@ -1,16 +1,49 @@
 import React from "react";
 import Layout from "../components/Layout";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useMutation } from "react-query";
+import { useForm } from "react-hook-form";
+import { apiPostUserRegiser } from "../api";
 
 export default function SignUp() {
+  const navigate = useNavigate()
+  const {mutate} = useMutation(apiPostUserRegiser, {
+    onSuccess : (data) => {
+      if(data.result === true) {
+        navigate('/login');
+      }
+    },
+    onSettled: (data) => {
+      if(data.result === false) {
+        setError(`user_id`, {
+          message: data.message,
+        })
+        setError(`password1`, {
+          message: data.message
+        })
+      }
+    }
+  });
+  const {register, handleSubmit,setError, formState:{errors}} = useForm();
+  const onValid = (data) => mutate(data)
   return (
     <Layout>
       <div className="w-full flex flex-col items-center py-16">
         <span className="text-3xl font-semibold mb-7">회원가입</span>
-        <form action="" className="flex flex-col gap-5 mb-2">
-          <input name="id" className="w-[300px] h-[50px] rounded-lg border-none ring-1  ring-gray-300 outline-none focus:ring-2 focus:ring-[#119724]" type="text" placeholder="아이디" />
-          <input name="password" className="w-[300px] h-[50px] rounded-lg border-none ring-1 ring-gray-300 outline-none focus:ring-2 focus:ring-[#119724]" type="password" placeholder="비밀번호" />
-          <input name="password2" className="w-[300px] h-[50px] rounded-lg border-none ring-1 ring-gray-300 outline-none focus:ring-2 focus:ring-[#119724]" type="password" placeholder="비밀번호 확인" />
+        <form onSubmit={handleSubmit(onValid)} action="" className="flex flex-col gap-5 mb-2">
+          <input {...register("user_id")} name="user_id" className="w-[300px] h-[50px] rounded-lg border-none ring-1  ring-gray-300 outline-none focus:ring-2 focus:ring-[#119724]" type="text" placeholder="아이디" />
+          {errors && (
+            <span className=" absolute -bottom-5 left-0 text-red-500 text-sm">
+                {errors?.user_id?.message}
+            </span>
+          )}
+          <input {...register("password1")} name="password1" className="w-[300px] h-[50px] rounded-lg border-none ring-1 ring-gray-300 outline-none focus:ring-2 focus:ring-[#119724]" type="password" placeholder="비밀번호" />
+          {errors && (
+            <span className=" absolute -bottom-5 left-0 text-red-500 text-sm">
+                {errors?.password1?.message}
+            </span>
+          )}
+          <input {...register("password2")} name="password2" className="w-[300px] h-[50px] rounded-lg border-none ring-1 ring-gray-300 outline-none focus:ring-2 focus:ring-[#119724]" type="password" placeholder="비밀번호 확인" />
           <button className="w-[300px] h-[50px] bg-[#119724] rounded-lg flex justify-center items-center text-xl text-white cursor-pointer">가입하기</button>
         </form>
         <Link to="/login">이미 계정이 있다면 로그인</Link>
