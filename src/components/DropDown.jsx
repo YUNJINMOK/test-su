47c64934e137baa9864cb595e26f47024270b109
ldh <ai04120@naver.com>
2024-05-10 16:02:10 +0900

    1

--- src/components/DropDown.jsx
+++ src/components/DropDown.jsx
@@ -1,15 +1,26 @@
 import React, { useState, useEffect, useRef } from "react";
 import { IoMdArrowDropdown } from "react-icons/io";
 
+function DropdownBtn({ onClick, text1, text2, border }) {
+  return (
+    <button onClick={onClick} className={`w-full px-4 py-3 text-gray-700 grid grid-cols-[1fr_185px] justify-center items-center gap-x-4 ${border} border-white text-lg`} role="menuitem">
+      <div className="w-[60px] h-[60px] bg-white rounded-full justify-self-end"></div>
+      <div className="text-left">
+        <p className="font-semibold">{text1}</p>
+        <p>{text2}</p>
+      </div>
+    </button>
+  );
+}
 
 export default function DropdownMenu() {
-  const [selectedMenu, setSelectedMenu] = useState("보상");
+  const [selectedMenu, setSelectedMenu] = useState(["asdf", "zxcv"]);
   const [menuOpen, setMenuOpen] = useState(false);
   const menuRef = useRef(null);
 
   //   메뉴 선택하면 닫히게
-  const handleMenuClick = (menu) => {
-    setSelectedMenu(menu);
+  const handleMenuClick = (text) => {
+    setSelectedMenu(text);
     setMenuOpen(false);
   };
 
@@ -29,48 +40,29 @@ export default function DropdownMenu() {
   }, []);
 
   return (
-    <div className="relative mt-[20px] inline-block text-left" ref={menuRef}>
+    <div className="relative my-7 inline-block text-left" ref={menuRef}>
       <div>
         <button
           type="button"
-          className="relative inline-flex justify-center items-center rounded-md w-[310px] px-2 py-5 bg-[#E6E6E6] font-bold text-[30px] text-[#8B8B8B]"
+          className="relative grid grid-cols-[1fr_190px] justify-center items-center rounded-md w-[310px] px-2 py-5 bg-[#E6E6E6] text-lg gap-x-4 "
           onClick={() => setMenuOpen(!menuOpen)} // 버튼 클릭 시 메뉴 열기/닫기 토글
         >
-          {selectedMenu}
-          <IoMdArrowDropdown className="absolute right-2"/>
+          <div className="w-[60px] h-[60px] bg-white rounded-full justify-self-end"></div>
+          <div className="text-left">
+            <p className="font-semibold">{selectedMenu[0]}</p>
+            <p>{selectedMenu[1]}</p>
+          </div>
+          <IoMdArrowDropdown className="absolute right-2" />
         </button>
       </div>
 
       {/* 드롭다운 메뉴 */}
       {menuOpen && (
-        <div className="origin-top-right absolute right-0 w-full rounded-md shadow-lg bg-white">
-          <div
-            className="py-1"
-            role="menu"
-            aria-orientation="vertical"
-            aria-labelledby="options-menu"
-          >
-            <button
-              onClick={() => handleMenuClick("보상")}
-              className="w-full block px-4 py-2 text-sm text-gray-700"
-              role="menuitem"
-            >
-              보상
-            </button>
-            <button
-              onClick={() => handleMenuClick("완료 스탬프")}
-              className="w-full block px-4 py-2 text-sm text-gray-700"
-              role="menuitem"
-            >
-              완료 스탬프
-            </button>
-            <button
-              onClick={() => handleMenuClick("미완료 스탬프")}
-              className="w-full block px-4 py-2 text-sm text-gray-700"
-              role="menuitem"
-            >
-              미완료 스탬프
-            </button>
+        <div className="origin-top-right absolute right-0 w-full rounded-md shadow-lg bg-[#e6e6e6] mt-1">
+          <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+            <DropdownBtn onClick={() => handleMenuClick(["스탬프 1개", "생분해 대나무 칫솔"])} text1="스탬프 1개" text2="생분해 대나무 칫솔" border="border-b" />
+            <DropdownBtn onClick={() => handleMenuClick(["스탬프 3개", "친환경 설거지 비누"])} text1="스탬프 3개" text2="친환경 설거지 비누" border="border-b" />
+            <DropdownBtn onClick={() => handleMenuClick(["스탬프 6개", "업사이클링 에코백"])} text1="스탬프 6개" text2="업사이클링 에코백" />
           </div>
         </div>
       )}
--- src/router/IndoorInfo.jsx
+++ src/router/IndoorInfo.jsx
@@ -4,6 +4,7 @@ import "../style/indoorinfo.css";
 import Title from "../components/Title";
 import Hours from "../components/Hours";
 import GreenDot from "../components/GreenDot";
+import { Link } from "react-router-dom";
 
 export default function IndoorInfo() {
   return (
@@ -27,21 +28,19 @@ export default function IndoorInfo() {
             <GreenDot title="층별 안내" />
             <div>
               <div className="floorInfo-con-box">
-                <p>1층</p>
+                <p>
+                  1층 <p>비지터 센터, 제1전시실, 작은 도서관</p>
+                </p>
                 <div className="floorImg">
                   <div></div>
-                  <div></div>
-                  {/* <img src="" alt="이미지1" />
-                  <img src="" alt="이미지2" /> */}
                 </div>
               </div>
               <div className="floorInfo-con-box">
-                <p>2층</p>
+                <p>
+                  2층<p>제2전시실, 기획전시실</p>
+                </p>
                 <div className="floorImg">
                   <div></div>
-                  <div></div>
-                  {/* <img src="" alt="이미지1" />
-                  <img src="" alt="이미지2" /> */}
                 </div>
               </div>
             </div>
@@ -56,8 +55,45 @@ export default function IndoorInfo() {
           {/* 이용시간 */}
           <div className="intro-time">
             <GreenDot title="이용 시간" />
-            <Hours date1="0월 0일~0월 0일" time1="0시간" date2="0월 0일~0월 0일" time2="0시간" />
+            <Hours date1="3월 1일 ~ 10월 31일" time1="오전 9시 ~ 오후 5시" date2="11월 1일 ~ 2월 28일" time2="오전 9시 ~ 오후 4시" />
             <div className="timetable-caution">※매주 월요일과 공휴일은 휴관일입니다.</div>
+          </div>
+          {/* 이용안내 */}
+          <div className="useProgInfo">
+            <GreenDot title="목공 체험 안내" />
+            <div className="progInnerInfo">
+              <p className="progTit">체험프로그램 참여 방법</p>
+              <p className="mb-6">
+                - <Link to="https://yeyak.daegu.go.kr/yeyak/experience/experienceListView.do?memId=B0000005">대구광역시 통합예약시스템</Link>에서 3일전까지 예약
+              </p>
+              <p className="progTit">참여 비용</p>
+              <p>- 성인: 2,000원</p>
+              <p>- 미성년자: 1,000원</p>
+              <p>※현장 카드 결제만 가능하며, 재료비는 별도입니다.</p>
+              <p>※20명 이상 예약시 개인체험료의 20%가 할인됩니다.</p>
+            </div>
+          </div>
+          {/* 층별안내 */}
+          <div className="floorInfo">
+            <GreenDot title="층별 안내" />
+            <div>
+              <div className="floorInfo-con-box">
+                <p>
+                  1층 <p>유아체험장· 유아놀이방, 초등체험장</p>
+                </p>
+                <div className="floorImg">
+                  <div></div>
+                </div>
+              </div>
+              <div className="floorInfo-con-box">
+                <p>
+                  2층<p>중등체험장, 성인체험장</p>
+                </p>
+                <div className="floorImg">
+                  <div></div>
+                </div>
+              </div>
+            </div>
           </div>
         </article>
       </div>
