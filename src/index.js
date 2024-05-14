1756ad587160b8c472e7bb4e170f1eadf76a2d0d
zxwgm <aprnaprn114@naver.com>
2024-05-14 14:31:06 +0900

    re

--- src/index.js
+++ src/index.js
@@ -30,8 +30,8 @@ const router = createBrowserRouter([
     errorElement: <Notfound />,
     children: [
       {
-        path: "",
-        element: <Home />,
+        path: "/",
+        element: <Home2 />,
       },
       {
         path: "/home",
