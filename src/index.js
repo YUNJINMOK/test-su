4e93c74fc5416c5827b2454a3fee0713180e4a74
ldh <ai04120@naver.com>
2024-05-14 13:32:07 +0900

    1

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: "",
-        element: <MainPage />,
+        element: <MainPaHoge />,
       },
       {
         path: "/home",
