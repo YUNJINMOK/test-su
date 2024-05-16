8b52a1925a4d43bddaaa82e2021d85abb180129e
zxwgm <aprnaprn114@naver.com>
2024-05-16 15:14:30 +0900

    제발

--- src/router/QrPage.jsx
+++ src/router/QrPage.jsx
@@ -2,19 +2,22 @@ import React, { useEffect, useState } from "react";
 import "../style/qrpage.css";
 import { IoIosArrowBack } from "react-icons/io";
 import { Link } from "react-router-dom";
+import jsQR from "jsqr";
 
 export default function QrPage() {
   const [permissionGranted, setPermissionGranted] = useState(false);
+  const [qrData, setQrData] = useState(null);
   console.log(permissionGranted);
+
   useEffect(() => {
     const requestCameraPermission = async () => {
       try {
         const stream = await navigator.mediaDevices.getUserMedia({
           video: true,
         });
-        console.log(stream);
         // 카메라 액세스 허용됨
         setPermissionGranted(true);
+        startQrScanning(stream);
       } catch (error) {
         // 권한 거부 또는 오류 발생
         console.error("카메라 액세스 거부:", error);
@@ -23,6 +26,43 @@ export default function QrPage() {
 
     requestCameraPermission();
   }, []);
+
+  const startQrScanning = (stream) => {
+    const video = document.createElement("video");
+    video.srcObject = stream;
+    video.setAttribute("playsinline", true);
+    video.play();
+
+    const canvasElement = document.getElementById("canvas");
+    const canvas = canvasElement.getContext("2d");
+
+    const scan = () => {
+      if (video.readyState === video.HAVE_ENOUGH_DATA) {
+        canvasElement.height = video.videoHeight;
+        canvasElement.width = video.videoWidth;
+        canvas.drawImage(
+          video,
+          0,
+          0,
+          canvasElement.width,
+          canvasElement.height
+        );
+        const imageData = canvas.getImageData(
+          0,
+          0,
+          canvasElement.width,
+          canvasElement.height
+        );
+        const code = jsQR(imageData.data, imageData.width, imageData.height);
+        if (code) {
+          setQrData(code.data);
+        }
+      }
+      requestAnimationFrame(scan);
+    };
+    requestAnimationFrame(scan);
+  };
+
   return (
     <div className="qrSection">
       <Link to="/home" className="qrArrow">
@@ -30,8 +70,11 @@ export default function QrPage() {
       </Link>
       <p className="qrText">QR 코드를 촬영해주세요</p>
       <div className="qrZone">
-        <p>화면을 인식 중입니다</p>
-        <canvas id="canvas" width="300" height="300"></canvas>
+        {qrData ? (
+          <p>QR 코드 데이터: {qrData}</p>
+        ) : (
+          <canvas id="canvas" width="300" height="300"></canvas>
+        )}
       </div>
     </div>
   );
