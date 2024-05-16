e05acd62717e1f76ad498f37f029ca0f8d41035c
zxwgm <aprnaprn114@naver.com>
2024-05-16 13:20:42 +0900

    re

--- src/router/QrPage.jsx
+++ src/router/QrPage.jsx
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../style/qrpage.css";
 import { IoIosArrowBack } from "react-icons/io";
 import { Link } from "react-router-dom";
 
 export default function QrPage() {
-  navigator.mediaDevices
-    .getUserMedia({ video: true })
-    .then(function (stream) {
-      // 카메라 액세스 허용됨
-    })
-    .catch(function (error) {
-      // 권한 거부 또는 오류 발생
-      console.error("카메라 액세스 거부:", error);
-    });
+  const [permissionGranted, setPermissionGranted] = useState(false);
+
+  useEffect(() => {
+    const requestCameraPermission = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: true,
+        });
+        // 카메라 액세스 허용됨
+        setPermissionGranted(true);
+      } catch (error) {
+        // 권한 거부 또는 오류 발생
+        console.error("카메라 액세스 거부:", error);
+      }
+    };
+
+    requestCameraPermission();
+  }, []);
   return (
     <div className="qrSection">
       <Link to="/home" className="qrArrow">
